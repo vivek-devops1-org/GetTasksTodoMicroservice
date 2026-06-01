7525a99fce5e0c66b0960b085337f717a9437c3f
Vivek <30vivekgoyal@gmail.com>
2026-06-01 22:02:07 +0530

    update gettask

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 
 # Fetch the connection string from the environment variable
-connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:monotodo.database.windows.net,1433;Database=free-sql-db-7549392;Uid=monosql;Pwd=Dockervm@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:monotodo1.database.windows.net,1433;Database=free-sql-db-7549392;Uid=monosql;Pwd=Dockervm@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 
 # Check if the connection string is available
 if connection_string:
